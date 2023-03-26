a4c66b1bd59c12d764d0cf7921112bec258d15b5
Fraymotiff <68887087+Fraymotiff@users.noreply.github.com>
2023-03-26 19:05:10 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -11,8 +11,14 @@ setInterval( function(){
 
 
 
-
-
-
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
 
 
