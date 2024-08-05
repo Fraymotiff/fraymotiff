0ad664c0501ae3437fec3bb7cf2b7a1621034ec2
Lua Jaxuka Morel <68887087+Fraymotiff@users.noreply.github.com>
2024-08-05 02:21:18 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,75 @@
+// MOBILE NAV
+
+const div = document.querySelector("#mobile");
+const div = document.querySelector(".btn-menu");
+const div = document.querySelector(".menu");
+
+
+function handleButtonClick(event){
+
+  div.classList.toggle ("active")
+
+handleClickOutside (menu, () => {
+
+  div.classList.remove("active");
+
+});
+}
+
+function handleClickOutside( targetElement, callback) {
+
+  const html = document.documentElement;
+
+function handleHTMLClick (event) {
+
+  if (!targetElement.contains(event.target)) {
+
+    targetElement.removeAttribute("data-target");
+    html.removeEventListener("touchstart", handleHTMLClick);
+
+    callback()
+  }
+}
+
+  if (!targetElement.hasAttribute("data-target")) {
+    html.addEventListener("touchstart", handleHTMLClick);
+
+
+
+
+targetElement.hasAttribute("data-target", "");
+
+  }
+
+}
+
+
+function setAria() {
+  const isActive = nav.classList.contains("active");
+  btnMenu.setAttribute("aria-expanded", isActive);
+  if (isActive) {
+    btnMenu.setAttribute("aria-label", "Fechar Menu");
+  } else {
+    btnMenu.setAttribute("aria-label", "Abrir Menu");
+  }
+}
+
+btnMenu.addEventListener("click", handleButtonClick);
+btnMenu.addEventListener("touchstart", handleButtonClick);
+
+
+
+
+
+
+
+
+
+
+// Mobile Nav
+
+// SLIDES
+
 let contador = 1;
 
 setInterval( function(){
@@ -10,15 +82,50 @@ setInterval( function(){
 }, 3000 );
 
 
+// SLIDES
 
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
+
+    const nav = document.querySelector(".nav");
+const btnMenu = document.querySelector(".btn-menu");
+const menu = document.querySelector(".menu");
+
+function handleButtonClick(event) {
+  if (event.type === "touchstart") event.preventDefault();
+  event.stopPropagation();
+  nav.classList.toggle("active");
+  handleClickOutside(menu, () => {
+    nav.classList.remove("active");
+    setAria();
+  });
+  setAria();
+}
+
+function handleClickOutside(targetElement, callback) {
+  const html = document.documentElement;
+  function handleHTMLClick(event) {
+    if (!targetElement.contains(event.target)) {
+      targetElement.removeAttribute("data-target");
+      html.removeEventListener("click", handleHTMLClick);
+      html.removeEventListener("touchstart", handleHTMLClick);
+      callback();
     }
   }
+  if (!targetElement.hasAttribute("data-target")) {
+    html.addEventListener("click", handleHTMLClick);
+    html.addEventListener("touchstart", handleHTMLClick);
+    targetElement.setAttribute("data-target", "");
+  }
+}
 
+function setAria() {
+  const isActive = nav.classList.contains("active");
+  btnMenu.setAttribute("aria-expanded", isActive);
+  if (isActive) {
+    btnMenu.setAttribute("aria-label", "Fechar Menu");
+  } else {
+    btnMenu.setAttribute("aria-label", "Abrir Menu");
+  }
+}
 
+btnMenu.addEventListener("click", handleButtonClick);
+btnMenu.addEventListener("touchstart", handleButtonClick);
